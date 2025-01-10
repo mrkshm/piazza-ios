e7fabd8eecfd70adffedaffaf88cb01456b74e34
mrks_dev <mrks.heumann@gmail.com>
2025-01-10 13:55:30 +0100

    Initial Commit

---
+++ Piazza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Piazza
+//
+//  Created by Marc Haussmann on 10/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PiazzaTests/PiazzaTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiazzaTests.swift
+//  PiazzaTests
+//
+//  Created by Marc Haussmann on 10/01/2025.
+//
+
+import Testing
+@testable import Piazza
+
+struct PiazzaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
